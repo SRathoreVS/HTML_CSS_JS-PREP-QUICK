f14f8793c6795020fa495e6300a14a7cf715155e
Satyam Rathore <136310276+SRathoreVS@users.noreply.github.com>
2024-12-25 04:40:42 +0000

    DOM-Selectors

---
+++ JAVASCRIPT/script3.js
@@ -0,0 +1,23 @@
+// DOM -->> Document Object Model
+
+// getElementByTagName
+let a = document.getElementsByTagName("li");
+console.log(a);
+
+// getElementByClassName
+let b = document.getElementsByClassName("hero");
+console.log(b);
+
+//getElementById
+let c = document.getElementById("jD");
+console.log(c);
+
+//querySelector
+let d = document.querySelector(".hero");
+let e = document.querySelector("#jB");
+console.log(d, e);
+
+//querySelectorAll
+
+let f = document.querySelectorAll(".hero");
+console.log(f);
