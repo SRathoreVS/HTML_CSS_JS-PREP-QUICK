3bc84e7baefd8c4eda5a2879aa2f0f5d6a774522
Satyam Rathore <136310276+SRathoreVS@users.noreply.github.com>
2024-12-25 20:11:36 +0000

    forms

---
+++ JAVASCRIPT/Forms/form.js
@@ -0,0 +1,12 @@
+let input = document.getElementById("input");
+
+const onFun = () => {
+  input.style.backgroundColor = "red";
+};
+const onBun = () => {
+  input.style.backgroundColor = "yellow";
+};
+
+const onSub = () => {
+  alert(`${input.value}`);
+};
