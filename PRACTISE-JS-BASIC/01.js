84b66264b26ed35b81b794057850b0e4ed8455d7
Satyam Rathore <136310276+SRathoreVS@users.noreply.github.com>
2024-12-25 20:20:04 +0000

    hello - loop

---
+++ PRACTISE-JS-BASIC/01.js
@@ -0,0 +1,27 @@
+//Hello -world
+
+console.log("hello World");
+
+let hello = "hello world";
+console.log(hello);
+
+//loop to print num from 1-5
+
+//for
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//while
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+//do-while
+let j = 1
+do {
+    console.log(j);
+    j++
+} while (j <= 5);
